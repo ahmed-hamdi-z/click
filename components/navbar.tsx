6abcf2e48d866516ed4d18ce8b3cef551434388f
Ahmed Zedan <ahmedhamdizedan@gmail.com>
2024-09-18 22:44:49 +0300

    add btn to nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -34,42 +34,42 @@ const Navbar = () => {
   }, []);
   return (
 
+    <div
+      className={
+        nav
+          ? "bg-[#1F1530] font-aj bg-opacity-95s p-6 h-4 fixed w-full border-b-[1px] border-[#ceecf7]  md:p-10 flex items-center justify-between z-40"
+          : "bg-transparent font-aj p-6 h-4 fixed w-full border-b-[1px] border-[#ceecf7] border-opacity-50  md:p-10 flex items-center justify-between z-40"
+      }
+    >
+      <Link href='/' className="flex justify-center text-teal-600 sm:justify-start cursor-pointer">
+
+        <Image width={100} height={50} src='/images/logo.svg' alt="" />
+      </Link>
       <div
-        className={
-          nav
-            ? "bg-[#1F1530] font-aj bg-opacity-95s p-6 h-4 fixed w-full border-b-[1px] border-[#ceecf7]  md:p-10 flex items-center justify-between z-40"
-            : "bg-transparent font-aj p-6 h-4 fixed w-full border-b-[1px] border-[#ceecf7] border-opacity-50  md:p-10 flex items-center justify-between z-40"
-        }
-      >
-     <Link href='/' className="flex justify-center text-teal-600 sm:justify-start cursor-pointer">
-      
-         <Image width={100} height={50} src='/images/logo.svg' alt="" />
-        </Link> 
-        <div
-          className="cursor-pointer hidden text-xl
+        className="cursor-pointer hidden text-xl
             md:flex space-x-10 items-center
             text-[#ceecf7] text-center "
-        >
-          <Link href="#mission" className="hover:text-[#f0c363]">
-            المهمة
-          </Link>
-          <Link href="#mission" className="hover:text-[#f0c363]">
-            الرؤية
-          </Link>
+      >
+        <Link href="#mission" className="hover:text-[#f0c363]">
+          المهمة
+        </Link>
+        <Link href="#mission" className="hover:text-[#f0c363]">
+          الرؤية
+        </Link>
 
-          <Link href="#services" className="hover:text-[#f0c363]">
-            خدماتنا
-          </Link>
-          <Link href="#about" className="hover:text-[#f0c363]">
-            من نحن
-          </Link>
+        <Link href="#services" className="hover:text-[#f0c363]">
+          خدماتنا
+        </Link>
+        <Link href="#about" className="hover:text-[#f0c363]">
+          من نحن
+        </Link>
 
-          <Link href="/" className="hover:text-[#f0c363]">
-            الرئيسية
-          </Link>
-        </div>
-
-        <div className="flex md:hidden">
+        <Link href="/" className="hover:text-[#f0c363]">
+          الرئيسية
+        </Link>
+      </div>
+      <div className="flex flex-row-reverse items-center justify-center gap-6">
+        <div className="flex md:hidden ">
           {isDropDownVisible ? (
             // display an x icon when the drop is visible
             <div
@@ -87,13 +87,13 @@ const Navbar = () => {
           )}
         </div>
 
-        <div className="hidden md:flex">
+        <div className="flex">
           <Link
             href="/contact"
             className="
-            inline-flex h-12 animate-shimmer items-center justify-center 
+            inline-flex h-10 animate-shimmer items-center justify-center 
             rounded-md border border-slate-800 bg-[linear-gradient(110deg,#1F1530,45%,#ceecf7,55%,#f0c363)] 
-            bg-[length:200%_100%] px-6 font-medium text-[#ceecf7] transition-colors
+            bg-[length:200%_100%] px-4 font-medium text-[#ceecf7] transition-colors
              focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2
               focus:ring-offset-slate-50
             "
@@ -102,6 +102,8 @@ const Navbar = () => {
           </Link>
         </div>
       </div>
+
+    </div>
 
   );
 };
